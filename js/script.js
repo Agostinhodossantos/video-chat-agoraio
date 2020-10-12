7dad483a558930d446550bc83966f64dccc7ea69
AgostinhoDossantos <agostinhodossantos27@gmail.com>
2020-10-12 01:04:32 -0700

    init ui elements

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let handleFail = function(err){
+    console.log("Err : ", err);
+};
+
+let remoteContainer = document.getElementById('remote-container');
+let canvasContainer = document.getElementById("canvas-container");
+
